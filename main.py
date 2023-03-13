51f5fb617c3c01dbae7cd07aa2ea27e5bffa8ddc
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-03-13 16:39:06 +0100

    libreria

---
+++ main.py
@@ -0,0 +1,6 @@
+# Importamos las librería threading para poder usar los hilos
+from threading import Thread, Lock, Event
+import time # Importamos la librería time para poder usar el sleep
+import random  # Importamos la librería random para poder usar el randrange
+from colorama import Fore # Importamos la librería colorama para poder usar los colores
+
